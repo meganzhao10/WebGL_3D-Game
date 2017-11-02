a8141a583bbd10b2b37d9e3011eeba3664f9fd73
meganzhao <zhaom@carleton.edu>
2017-11-02 15:22:10 +0100

    add rotor to helicoptor

--- Src/js/GameObject.js
+++ Src/js/GameObject.js
@@ -1,13 +1,14 @@
 "use strict"; 
 let GameObject = function(mesh) { 
   this.mesh = mesh;
+  this.parent = null;
   this.position = new Vec3(0, 0, 0); 
   this.orientation = 0; 
   this.scale = new Vec3(0.45, 0.45, 1); 
   this.angularVelocity = 0;
   this.modelMatrix = new Mat4(); 
-
 };
+
 
 GameObject.prototype.updateModelMatrix = function(){ 
 // TODO: set the model matrix according to the 
@@ -18,23 +19,24 @@ GameObject.prototype.updateModelMatrix = function(){
     rotate(this.angularVelocity).
     rotate(this.orientation).
     translate(this.position);
+
 };
 
 GameObject.prototype.draw = function(camera,lightSource){ 
   this.updateModelMatrix();
 // TODO: Set the uniform modelViewProjMatrix (reflected in the material) to modelMatrix multiplied by the camera’s viewProjMatrix. Use Mat4 methods set() and/or mul().
   //this.mesh.setUniform("modelViewProjMatrix",this.modelMatrix.mul(camera.viewProjMatrix));
-  
-  Material.modelViewProjMatrix.set(this.modelMatrix).mul(camera.viewProjMatrix);
+
+  Material.modelViewProjMatrix.set(this.modelMatrix);
+  if (this.parent != null){
+  	Material.modelViewProjMatrix.mul(this.parent.modelMatrix);
+  }
+  Material.modelViewProjMatrix.mul(camera.viewProjMatrix);
+
   Material.modelMatrix.set(this.modelMatrix);
   Material.modelMatrixInverse.set(this.modelMatrix).invert();
   Material.lightPos.set(lightSource.lightPos);
   Material.powerDensity.set(lightSource.powerDensity);
-  //this.mesh.setUniform("lightPos",lightSource);
-  // this.mesh.material.modelViewProjMatrix.set().
-  // 	mul(this.modelMatrix).
-  //   mul(camera.viewProjMatrix);
-  //Material.lightPos[0].set(0.5,0.7,0.3);
   this.mesh.draw(); 
 };
 
--- Src/js/Scene.js
+++ Src/js/Scene.js
@@ -6,7 +6,7 @@ let Scene = function(gl) {
   //this.TexturedQuadGeometry = new TexturedQuadGeometry(gl);
 
   this.gameObjects = [];
-
+  //car object
   this.carMaterial = new Material(gl,this.textureProgram);
   this.carTexture = new Texture2D(gl, 'json/chevy/chevy.png');
   this.carMaterial.colorTexture.set(this.carTexture.glTexture);
@@ -14,6 +14,25 @@ let Scene = function(gl) {
   this.carObject = new GameObject(this.carMesh);
   this.gameObjects.push(this.carObject);
 
+  //car wheels
+  this.wheel1Mesh = new MultiMesh(gl,"json/chevy/wheel.json",[this.carMaterial])
+  this.wheel1Object = new GameObject(this.wheel1Mesh);  
+  this.wheel1Object.parent = this.carObject;
+  this.gameObjects.push(this.wheel1Object);
+
+  // this.wheel2Object = new GameObject(this.mainrotorMesh);  
+  // this.mainrotorObject.parent = this.heli1Object;
+  // this.gameObjects.push(this.mainrotorObject);
+
+  //   this.mainrotorObject = new GameObject(this.mainrotorMesh);  
+  // this.mainrotorObject.parent = this.heli1Object;
+  // this.gameObjects.push(this.mainrotorObject);
+
+  //   this.mainrotorObject = new GameObject(this.mainrotorMesh);  
+  // this.mainrotorObject.parent = this.heli1Object;
+  // this.gameObjects.push(this.mainrotorObject);
+
+  //helicoptor
   this.heli1Material = new Material(gl,this.textureProgram);
   this.heli1Texture = new Texture2D(gl, 'json/heli/heli.png');
   this.heli1Material.colorTexture.set(this.heli1Texture.glTexture);
@@ -21,12 +40,19 @@ let Scene = function(gl) {
   this.heli1Object = new GameObject(this.heli1Mesh);  
   this.gameObjects.push(this.heli1Object);
 
+  this.mainrotorMesh = new MultiMesh(gl,"json/heli/mainrotor.json",[this.heli1Material, this.heli1Material]);
+  this.mainrotorObject = new GameObject(this.mainrotorMesh);  
+  this.mainrotorObject.parent = this.heli1Object;
+  this.gameObjects.push(this.mainrotorObject);
+
 
   this.lightSource = new LightSource();
   this.lightSource.lightPos = new Vec4Array(1);
-  this.lightSource.lightPos.at(0).set(0,0,1,0);
+  this.lightSource.lightPos.at(0).set(0,0,1,0); // the last 0 indicates that it's a directional light
   this.lightSource.powerDensity = new Vec4Array(1);
   this.lightSource.powerDensity.at(0).set(0.5,0.5,0.5,0.5);  
+  //powerDensity for directional light between 0 and 1
+  //powerDensity for point light (10, 100, 1000,1), if white surface with this source, would be mostly blue, things that are close to it will be green, things really close will be white
 
   this.camera = new PerspectiveCamera();
 };
@@ -37,6 +63,8 @@ Scene.prototype.update = function(gl, keysPressed) {
   let dt = (timeAtThisFrame - this.timeAtLastFrame) / 1000.0;
   this.timeAtLastFrame = timeAtThisFrame;
 
+  let speed = 0.5;
+
   this.camera.move(dt,keysPressed);
 
   // clear the screen
@@ -45,16 +73,23 @@ Scene.prototype.update = function(gl, keysPressed) {
   gl.clearDepth(1.0);
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
+  this.mainrotorObject.position.set(0.5,15,0);
+  this.wheel1Object.position.set(4,-5,4);
+
   for (var i = 0; i < this.gameObjects.length; i++){
       this.gameObjects[i].draw(this.camera, this.lightSource);
-      this.gameObjects[i].scale.set(0.005,0.005,0.005);
-      this.gameObjects[i].orientation = 1.5;
+      if(this.gameObjects[i].parent == null){
+        this.gameObjects[i].scale.set(0.005,0.005,0.005);
+      }
+      this.gameObjects[i].orientation = 0;
   }
 
-      // this.gameObjects[1].draw(this.camera, this.lightSource);
-      // this.gameObjects[1].scale.set(0.005,0.005,0.005);
-      // this.gameObjects[1].orientation = 1.5;
-
+  if(keysPressed.LEFT) { 
+    this.heli1Object.position.add(-speed * dt,0,0); 
+  } 
+  if(keysPressed.RIGHT) { 
+    this.heli1Object.position.add(speed * dt,0,0); 
+  } 
 }
 
 
